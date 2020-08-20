459ea10c59cbbca8356880d1ad06d9a331d099d0
nsmanasi <mansiprakashsharma@yahoo.co.in>
2020-08-20 23:55:12 +0530

    first commit

---
+++ Whileloop.py
@@ -0,0 +1,15 @@
+# Program for printing numbers from 1 to 10 using while loop
+
+#count = 1
+#while (count<=10):
+ #print(count)
+ #count = count+1
+
+count = 1
+sumnum=0
+while (count<=10):
+ print(count)
+ sumnum=sumnum+count
+ count = count+2
+print("The sum:",sumnum)
+
